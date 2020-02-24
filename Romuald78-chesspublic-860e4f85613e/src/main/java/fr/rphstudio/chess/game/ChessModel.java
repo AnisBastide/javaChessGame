285238c4b618b07bf70d331afbe30b023e842863
Anis <anis.bastide@gmail.com>
2020-02-24 14:05:53 +0100

    chess board visible

---
+++ Romuald78-chesspublic-860e4f85613e/src/main/java/fr/rphstudio/chess/game/ChessModel.java
@@ -0,0 +1,76 @@
+package fr.rphstudio.chess.game;
+
+import fr.rphstudio.chess.interf.EmptyCellException;
+import fr.rphstudio.chess.interf.IChess;
+import fr.rphstudio.chess.interf.OutOfBoardException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static fr.rphstudio.chess.interf.IChess.ChessColor.CLR_BLACK;
+import static fr.rphstudio.chess.interf.IChess.ChessKingState.KING_SAFE;
+import static fr.rphstudio.chess.interf.IChess.ChessType.TYP_PAWN;
+
+public class ChessModel implements IChess {
+private static ChessModel instance= new ChessModel();
+
+    private ChessModel(){
+
+    }
+    public static IChess getInstance(){
+
+        return ChessModel.instance;
+    }
+    @Override
+    public void reinit() {
+
+    }
+
+
+    @Override
+    public ChessType getPieceType(ChessPosition p) throws EmptyCellException, OutOfBoardException {
+        return TYP_PAWN;
+    }
+
+    @Override
+    public ChessColor getPieceColor(ChessPosition p) throws EmptyCellException, OutOfBoardException {
+        return CLR_BLACK;
+    }
+
+    @Override
+    public int getNbRemainingPieces(ChessColor color) {
+        return 0;
+    }
+
+    @Override
+    public List<ChessPosition> getPieceMoves(ChessPosition p) {
+        return new ArrayList<ChessPosition>();
+    }
+
+    @Override
+    public void movePiece(ChessPosition p0, ChessPosition p1) {
+
+    }
+
+    @Override
+    public ChessKingState getKingState(ChessColor color) {
+        return KING_SAFE;
+    }
+
+    @Override
+    public List<ChessType> getRemovedPieces(ChessColor color) {
+        return new ArrayList<ChessType>();
+    }
+
+    @Override
+    public boolean undoLastMove() {
+        return false;
+    }
+
+    @Override
+    public long getPlayerDuration(ChessColor color, boolean isPlaying) {
+        return 0;
+    }
+
+
+}
