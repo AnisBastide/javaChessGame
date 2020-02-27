e17946494fde7382dba42ff041da6d2d596cc771
Jimmy Lai <ljimmy98@hotmail.fr>
2020-02-27 15:00:48 +0100

    javadoc for MoveUtil

--- Romuald78-chesspublic-860e4f85613e/src/main/java/fr/rphstudio/chess/game/MoveUtil.java
+++ Romuald78-chesspublic-860e4f85613e/src/main/java/fr/rphstudio/chess/game/MoveUtil.java
@@ -6,40 +6,70 @@ import java.util.List;
 
 public class MoveUtil {
 
+    /**
+     * Check if the position is valid (in the board).
+     * @param pos position to check.
+     * @return a boolean.
+     */
     public static boolean isValidPosition(IChess.ChessPosition pos){
         if (pos.x <= 7 && pos.x >= 0 && pos.y <= 7 && pos.y >= 0){
             return true;
-        }else{
-            return false;
         }
+        return false;
+
     }
 
+    /**
+     * Check if the position is empty.
+     * @param pos position to check.
+     * @param board box setting (color, type, empty or not).
+     * @return a boolean.
+     */
     public static boolean isEmpty(IChess.ChessPosition pos, Board board){
         if (board.getPiece(pos) == null){
             return true;
-        }else{
-            return false;
         }
+        return false;
     }
 
+    /**
+     * Check if the position contain an enemy.
+     * @param p Position of the selected piece.
+     * @param pos position to check.
+     * @param board box setting (color, type, empty or not).
+     * @return a boolean.
+     */
     public static boolean isEnemy(IChess.ChessPosition p, IChess.ChessPosition pos, Board board){
         if (board.getPiece(pos).getColor() != board.getPiece(p).getColor()){
             return true;
-        }else{
-            return false;
         }
+        return false;
     }
 
+    /**
+     * Check if the position is empty or contain an enemy.
+     * @param p position of the selected piece.
+     * @param pos position to check.
+     * @param board box setting (color, type, empty or not).
+     * @return a boolean.
+     */
     public static boolean isEmptyOrEnemy(IChess.ChessPosition p, IChess.ChessPosition pos, Board board){
         if (board.getPiece(pos).getColor() != board.getPiece(p).getColor() || board.getPiece(pos) == null){
             return true;
-        }else{
-            return false;
         }
+        return false;
     }
 
     private static boolean check = true;
 
+    /**
+     * Check all position in diagonal.
+     * Set diagonal movements.
+     * @param p position of the selected piece.
+     * @param board box setting (color, type, empty or not).
+     * @param i limit of distance.
+     * @return a list of movement.
+     */
     public static List<IChess.ChessPosition> Diagonal(IChess.ChessPosition p, Board board, int i) {
         List<IChess.ChessPosition> list = new ArrayList<IChess.ChessPosition>();
         for (int dir = 0; dir <= 4; dir++) {
@@ -63,6 +93,14 @@ public class MoveUtil {
         return new ArrayList<>();
     }
 
+    /**
+     * Check all position in horizontal and vertical.
+     * Set horizontal and vertical movements.
+     * @param p position of the selected piece.
+     * @param board box setting (color, type, empty or not).
+     * @param i limit of distance.
+     * @return a list of movement.
+     */
     public static List<IChess.ChessPosition> Horizontal(IChess.ChessPosition p, Board board, int i){
         List<IChess.ChessPosition> list = new ArrayList<IChess.ChessPosition>();
         for (int dir = 0; dir <= 4; dir ++){
@@ -94,6 +132,14 @@ public class MoveUtil {
         return new ArrayList<>();
     }
 
+    /**
+     * Check if the position is possible.
+     * Add movements to the list
+     * @param p position of the selected piece.
+     * @param pos position to check and next move.
+     * @param board box setting (color, type, empty or not).
+     * @return a list of possible movements
+     */
     public static List<IChess.ChessPosition> PossibleMove(IChess.ChessPosition p, IChess.ChessPosition pos, Board board){
         List<IChess.ChessPosition> list = new ArrayList<IChess.ChessPosition>();
         if (pos.x <= 7 && pos.x >= 0 && pos.y <= 7 && pos.y >= 0 && check == true) {
@@ -114,6 +160,14 @@ public class MoveUtil {
         return list;
     }
 
+    /**
+     * Check if the position is possible for the Knight.
+     * Add movements of the Knight to the list.
+     * @param p position of the selected piece.
+     * @param pos position to check and next move.
+     * @param board box setting (color, type, empty or not).
+     * @return a list of possible movements
+     */
     public static List<IChess.ChessPosition> PossibleMoveKnight(IChess.ChessPosition p, IChess.ChessPosition pos, Board board){
         List<IChess.ChessPosition> list = new ArrayList<IChess.ChessPosition>();
         if (pos.x <= 7 && pos.x >= 0 && pos.y <= 7 && pos.y >= 0) {
@@ -130,6 +184,16 @@ public class MoveUtil {
         return list;
     }
 
+    /**
+     * Check if the position is possible.
+     * Set movements of Pawn when he's at his start position.
+     * Add movements of the Pawn to the list.
+     * @param p position of the selected piece.
+     * @param pos position to check and next move.
+     * @param board box setting (color, type, empty or not).
+     * @param dy normal movement of Pawn.
+     * @return a list of movements
+     */
     public static List<IChess.ChessPosition> MoveStartPawn(IChess.ChessPosition p, IChess.ChessPosition pos, Board board, int dy) {
         List<IChess.ChessPosition> list = new ArrayList<>();
         int cy = 0;
@@ -165,6 +229,16 @@ public class MoveUtil {
         return list;
     }
 
+    /**
+     * Check if the position is possible.
+     * Set movements of Pawn after he has move for the first time.
+     * Add movements of the Pawn to the list.
+     * @param p position of the selected piece.
+     * @param pos position to check and next move.
+     * @param board box setting (color, type, empty or not).
+     * @param dy normal movement of Pawn.
+     * @return a list of movements
+     */
     public static List<IChess.ChessPosition> MoveOtherPawn(IChess.ChessPosition p, IChess.ChessPosition pos, Board board, int dy) {
         List<IChess.ChessPosition> list = new ArrayList<>();
         int cy = 0;
